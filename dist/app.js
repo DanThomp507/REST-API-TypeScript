58512f43815fe2c9822eaac28e2fd7a3cb395329
Daniel Thompson <DanThomp507@gmail.com>
2019-06-26 10:08:07 -0400

    added statuscontroller end points

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
+const statusController = require("./controllers/status");
 const app = express();
 const PORT = process.env.PORT || 9000;
 app.set("port", process.env.PORT || 9000);
-app.get("/", (req, res) => {
-    res.send("Home page");
-});
+app.get("/", statusController.hello);
+app.post("/wonderful", statusController.wonderful);
 exports.default = app;
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import express = require("express");
 
+import * as statusController from "./controllers/status";
 const app = express();
 const PORT = process.env.PORT || 9000;
 
 app.set("port", process.env.PORT || 9000);
 
-app.get("/", (req, res) => {
-  res.send("Home page");
-});
+app.get("/", statusController.hello);
+app.post("/wonderful", statusController.wonderful);
 
 export default app;
